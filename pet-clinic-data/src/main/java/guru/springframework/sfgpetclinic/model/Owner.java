ea13c34a589289f2ebed706d475393cfae4c2e49
Yvalain <yves-alain-externe.eyoum@enedis.fr>
2020-01-08 11:11:50 +0100

    Added Create or Update a Pet. Closes #56

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -16,24 +16,41 @@ import java.util.Set;
 @Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
+    private String address;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "telephone")
+    private String telephone;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private Set<Pet> pets = new HashSet<>();
+
     @Builder
     public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
         super(id, firstName, lastName);
         this.address = address;
         this.city = city;
         this.telephone = telephone;
-        this.pets = pets;
+        if (pets != null) {
+            this.pets = pets;
+        }
     }
 
-    @Column(name = "address")
-    private String address;
+    public Pet getPet(String name) {
+        return getPet(name, false);
+    }
 
-    @Column(name = "city")
-    private String city;
-
-    @Column(name = "telephone")
-    private String telephone;
-
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
-    private Set<Pet> pets = new HashSet<>();
+    public Pet getPet(String name, boolean ignoreNew) {
+        name = name.toLowerCase();
+        for (Pet pet : pets) {
+            if (!ignoreNew || !pet.isNew()) {
+                String compName = pet.getName();
+                compName = compName.toLowerCase();
+                if (compName.equals(name)) {
+                    return pet;
+                }
+            }
+        }
+        return null;
+    }
 }
